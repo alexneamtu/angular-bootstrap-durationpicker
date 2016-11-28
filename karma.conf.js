1719777f9db890f3013f4a3908d1ad091d14cb4c
Alex Neamtu <alexneamtu@gmail.com>
2016-11-28 11:46:37 +0200

    PhantomJS tests

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function (config) {
         browsers: ['Chrome'],
         frameworks: ['jasmine'],
 
-        plugins : ['karma-jasmine', 'karma-chrome-launcher'],
+        plugins : ['karma-jasmine', 'karma-phantomjs-launcher'],
 
 
         captureTimeout: 60000,
